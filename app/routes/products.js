0d76fe704ad487cc07047971e00a6fcdbb1bed6e
elliot richardson <ear78@aol.com>
2017-09-28 23:38:51 -0500

    added dropdown menu and functionality on products page

--- app/routes/products.js
+++ app/routes/products.js
@@ -7,37 +7,43 @@ export default Ember.Route.extend({
                 id: 1,
                 title: 'ILS-3600-0IR',
                 description: 'Dual LED Lighting System w/Infrared',
-                image: '/assets/images/products/ILS-3600-000_CLOSED_1_T.png'
+                image: '/assets/images/products/ILS-3600-000_CLOSED_1_T.png',
+                category: 'Lighting Solutions'
             },
             {
                 id: 2,
                 title: 'ILS-3600-CAT-IR',
                 description: 'Medium Intensity Catenary Dual System',
-                image: '/assets/images/products/ILS-3600-CAT_T.png'
+                image: '/assets/images/products/ILS-3600-CAT_T.png',
+                category: 'Lighting Solutions'
             },
             {
                 id: 3,
                 title: 'ILS-2600-OIR',
                 description: 'White LED Lighting System w/Infrared',
-                image: '/assets/images/products/ILS-3600-000_CLOSED_1_T.png'
+                image: '/assets/images/products/ILS-3600-000_CLOSED_1_T.png',
+                category: 'Monitoring'
             },
             {
                 id: 4,
                 title: 'ILS-2600-CAT-IR',
                 description: 'Medium Intensity Catenary White System',
-                image: '/assets/images/products/ILS-3600-CAT_T.png'
+                image: '/assets/images/products/ILS-3600-CAT_T.png',
+                category: 'Monitoring'
             },
             {
                 id: 5,
                 title: 'ILS-D1RW-8SP',
                 description: 'Dual LED Lighting System',
-                image: '/assets/images/products/ILS-D1RW-8SP_CLOSED_1_T.png'
+                image: '/assets/images/products/ILS-D1RW-8SP_CLOSED_1_T.png',
+                category: 'ITL Monitoring Service'
             },
             {
                 id: 6,
                 title: 'IFH-1710-000',
                 description: 'Wind Turbine Lighting System',
-                image: '/assets/images/products/IFH-1710-000_CLOSED_1_T.png'
+                image: '/assets/images/products/IFH-1710-000_CLOSED_1_T.png',
+                category: 'ITL Monitoring Service'
             },
         ]
     }
